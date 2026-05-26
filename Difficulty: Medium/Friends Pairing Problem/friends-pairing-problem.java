bc6aada120626ac2774c498e21a8fb1f6bd624f5
Tushar Barik <tusharbarik64@gmail.com>
2026-05-26 23:48:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Friends Pairing Problem/friends-pairing-problem.java
+++ Difficulty: Medium/Friends Pairing Problem/friends-pairing-problem.java
@@ -3,14 +3,23 @@
 class Solution {
     
     public static long fp(int n,long[] dp){
-        if(n == 1) return 1;
+       if(n == 1) return 1;
         if(n == 2) return 2;
+        if(dp[n] != 0) return dp[n];
 
-        return fp(n-1,dp)+(n-1)*fp(n-2,dp);
+        return dp[n] = fp(n-1,dp)+(n-1)*fp(n-2,dp);
     }
     public long countFriendsPairings(int n) {
         // code here
-        long[] dp = new long[n];
-        return fp(n,dp);
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        long[] dp = new long[n+1];
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1] + (i-1)*dp[i-2];
+        }
+        return dp[n];
     }
 }
